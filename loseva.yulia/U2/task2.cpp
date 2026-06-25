1e62c1fe7fb5f468c2766effa5bfb41715a90083
yuklya <yuloseva@mail.ru>
2026-06-25 12:33:45 +0300

    fix cg task2

--- loseva.yulia/U2/task2.cpp
+++ loseva.yulia/U2/task2.cpp
@@ -5,17 +5,25 @@
 
 namespace loseva {
 
-bool parseArgsT2(int argc, char** argv, std::string& inFile, std::string& dataFile) {
-  if (argc < 2 || argc > 3) return false;
-  bool dataFound = false, inFound = false;
+bool parseArgsS2(const int argc, char** argv, std::string& inFile, std::string& dataFile)
+{
+  if (argc < 2 || argc > 3) {
+    return false;
+  }
+  bool dataFound = false;
+  bool inFound = false;
   for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
+    const std::string arg = argv[i];
     if (arg.find("data:") == 0) {
-      if (dataFound) return false;
+      if (dataFound) {
+        return false;
+      }
       dataFile = arg.substr(5);
       dataFound = true;
     } else if (arg.find("in:") == 0) {
-      if (inFound) return false;
+      if (inFound) {
+        return false;
+      }
       inFile = arg.substr(3);
       inFound = true;
     } else {
@@ -25,18 +33,25 @@ bool parseArgsT2(int argc, char** argv, std::string& inFile, std::string& dataFi
   return dataFound;
 }
 
-void loadMeets(const std::string& file, Array<Meet>& meets) {
+void loadMeets(const std::string& file, Array_t< Meet_t >& meets)
+{
   std::ifstream fin(file);
   if (!fin.is_open()) {
     std::cerr << "Open fail\n";
     std::exit(2);
   }
-  std::size_t id1, id2;
-  int time;
+  std::size_t id1 = 0;
+  std::size_t id2 = 0;
+  int time = 0;
   while (fin >> id1 >> id2 >> time) {
-    if (id1 == id2) continue;
-    Meet m; m.id1 = id1; m.id2 = id2; m.time = time;
-    push_back(meets, m);
+    if (id1 == id2) {
+      continue;
+    }
+    loseva::Meet_t m;
+    m.id1 = id1;
+    m.id2 = id2;
+    m.time = time;
+    loseva::push_back(meets, m);
   }
   if (fin.fail() && !fin.eof()) {
     std::cerr << "Format error\n";
@@ -44,7 +59,8 @@ void loadMeets(const std::string& file, Array<Meet>& meets) {
   }
 }
 
-void handleDesc(const Array<Person>& persons, std::size_t id) {
+void handleDesc(const Array_t< Person_t >& persons, const std::size_t id)
+{
   for (std::size_t i = 0; i < persons.size; ++i) {
     if (persons.data[i].id == id) {
       std::cout << persons.data[i].info << "\n";
@@ -56,24 +72,23 @@ void handleDesc(const Array<Person>& persons, std::size_t id) {
 
 }
 
-int main(int argc, char** argv) {
-  std::string inFile = "", dataFile = "";
-  if (!loseva::parseArgsT2(argc, argv, inFile, dataFile)) {
+int main(const int argc, char** argv)
+{
+  std::string inFile = "";
+  std::string dataFile = "";
+  if (!loseva::parseArgsS2(argc, argv, inFile, dataFile)) {
     std::cerr << "Invalid args\n";
     return 1;
   }
-  
-  loseva::Array<loseva::Person> persons;
+  loseva::Array_t< loseva::Person_t > persons;
   loseva::init(persons);
-  loseva::Array<loseva::Meet> meets;
+  loseva::Array_t< loseva::Meet_t > meets;
   loseva::init(meets);
-  
   loseva::loadMeets(dataFile, meets);
-  
-  std::string cmd;
+  std::string cmd = "";
   while (std::cin >> cmd) {
     if (cmd == "desc") {
-      std::size_t id;
+      std::size_t id = 0;
       if (std::cin >> id) {
         loseva::handleDesc(persons, id);
       } else {
@@ -83,10 +98,9 @@ int main(int argc, char** argv) {
     } else {
       std::cout << "<INVALID COMMAND>\n";
     }
-    std::string dummy;
+    std::string dummy = "";
     std::getline(std::cin, dummy);
   }
-  
   loseva::destroy(persons);
   loseva::destroy(meets);
   return 0;
