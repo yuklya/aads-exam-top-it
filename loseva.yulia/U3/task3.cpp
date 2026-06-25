89a01eb9821b0a6f35a13bb79d30d14ef0a3f00f
yuklya <yuloseva@mail.ru>
2026-06-25 13:14:18 +0300

    fix cg

--- loseva.yulia/U3/task3.cpp
+++ loseva.yulia/U3/task3.cpp
@@ -5,14 +5,17 @@
 
 namespace loseva {
 
-bool parseArgsT3(int argc, char** argv, std::string& inFile, Array<std::string>& dataFiles) {
+bool parseArgsS3(const int argc, char** argv, std::string& inFile, Array_t< std::string >& dataFiles)
+{
   bool inFound = false;
   for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
+    const std::string arg = argv[i];
     if (arg.find("data:") == 0) {
-      push_back(dataFiles, arg.substr(5));
+      loseva::push_back(dataFiles, arg.substr(5));
     } else if (arg.find("in:") == 0) {
-      if (inFound) return false;
+      if (inFound) {
+        return false;
+      }
       inFile = arg.substr(3);
       inFound = true;
     } else {
@@ -22,21 +25,31 @@ bool parseArgsT3(int argc, char** argv, std::string& inFile, Array<std::string>&
   return (dataFiles.size > 0);
 }
 
-void loadMeetsT3(const std::string& file, Array<MeetWithDate>& meets) {
+void loadMeetsS3(const std::string& file, Array_t< MeetWithDate_t >& meets)
+{
   std::ifstream fin(file);
   if (!fin.is_open()) {
     std::cerr << "Open fail\n";
     std::exit(2);
   }
-  int d, m, y;
-  std::size_t id1, id2;
-  int time;
+  int d = 0;
+  int m = 0;
+  int y = 0;
+  std::size_t id1 = 0;
+  std::size_t id2 = 0;
+  int time = 0;
   while (fin >> d >> m >> y >> id1 >> id2 >> time) {
-    if (id1 == id2) continue;
-    MeetWithDate mwd;
-    mwd.date.d = d; mwd.date.m = m; mwd.date.y = y;
-    mwd.meet.id1 = id1; mwd.meet.id2 = id2; mwd.meet.time = time;
-    push_back(meets, mwd);
+    if (id1 == id2) {
+      continue;
+    }
+    loseva::MeetWithDate_t mwd;
+    mwd.date.d = d;
+    mwd.date.m = m;
+    mwd.date.y = y;
+    mwd.meet.id1 = id1;
+    mwd.meet.id2 = id2;
+    mwd.meet.time = time;
+    loseva::push_back(meets, mwd);
   }
   if (fin.fail() && !fin.eof()) {
     std::cerr << "Format error\n";
@@ -44,7 +57,8 @@ void loadMeetsT3(const std::string& file, Array<MeetWithDate>& meets) {
   }
 }
 
-void calculateInitialPeriod(const Array<MeetWithDate>& meets, Period& prd) {
+void calculateInitialPeriod(const Array_t< MeetWithDate_t >& meets, Period_t& prd)
+{
   if (meets.size == 0) {
     prd.is_empty = true;
     return;
@@ -53,17 +67,17 @@ void calculateInitialPeriod(const Array<MeetWithDate>& meets, Period& prd) {
   prd.start = meets.data[0].date;
   prd.end = meets.data[0].date;
   for (std::size_t i = 1; i < meets.size; ++i) {
-    if (compare_dates(meets.data[i].date, prd.start) < 0) prd.start = meets.data[i].date;
-    if (compare_dates(meets.data[i].date, prd.end) > 0) prd.end = meets.data[i].date;
+    if (loseva::compare_dates(meets.data[i].date, prd.start) < 0) {
+      prd.start = meets.data[i].date;
+    }
+    if (loseva::compare_dates(meets.data[i].date, prd.end) > 0) {
+      prd.end = meets.data[i].date;
+    }
   }
 }
 
-bool dateInPeriod(const Date& dt, const Period& prd) {
-  if (prd.is_empty) return false;
-  return (compare_dates(dt, prd.start) >= 0 && compare_dates(dt, prd.end) <= 0);
-}
-
-void handleRange(const Period& prd) {
+void handleRange(const Period_t& prd)
+{
   if (prd.is_empty) {
     std::cout << "<EMPTY>\n";
   } else {
@@ -74,43 +88,43 @@ void handleRange(const Period& prd) {
 
 }
 
-int main(int argc, char** argv) {
+int main(const int argc, char** argv)
+{
   std::string inFile = "";
-  loseva::Array<std::string> dataFiles;
+  loseva::Array_t< std::string > dataFiles;
   loseva::init(dataFiles);
-  
-  if (!loseva::parseArgsT3(argc, argv, inFile, dataFiles)) {
+  if (!loseva::parseArgsS3(argc, argv, inFile, dataFiles)) {
     std::cerr << "Invalid args\n";
     loseva::destroy(dataFiles);
     return 1;
   }
-  
-  loseva::Array<loseva::Person> persons;
+  loseva::Array_t< loseva::Person_t > persons;
   loseva::init(persons);
-  loseva::Array<loseva::MeetWithDate> meets;
+  loseva::Array_t< loseva::MeetWithDate_t > meets;
   loseva::init(meets);
-  
   for (std::size_t i = 0; i < dataFiles.size; ++i) {
-    loseva::loadMeetsT3(dataFiles.data[i], meets);
+    loseva::loadMeetsS3(dataFiles.data[i], meets);
   }
-  
-  loseva::Period current_period;
+  loseva::Period_t current_period;
   loseva::calculateInitialPeriod(meets, current_period);
-  
-  loseva::RangeStack history;
+  loseva::RangeStack_t history;
   loseva::init(history);
-  
-  std::string cmd;
+  std::string cmd = "";
   while (std::cin >> cmd) {
     if (cmd == "range") {
       loseva::handleRange(current_period);
     } else if (cmd == "before") {
-      int d, m, y;
+      int d = 0;
+      int m = 0;
+      int y = 0;
       if (std::cin >> d >> m >> y) {
         if (current_period.is_empty) {
           std::cout << "<INVALID COMMAND>\n";
         } else {
-          loseva::Date limit; limit.d = d; limit.m = m; limit.y = y;
+          loseva::Date_t limit;
+          limit.d = d;
+          limit.m = m;
+          limit.y = y;
           loseva::push(history, current_period);
           if (loseva::compare_dates(current_period.start, limit) > 0) {
             current_period.is_empty = true;
@@ -123,12 +137,17 @@ int main(int argc, char** argv) {
         std::cin.clear();
       }
     } else if (cmd == "after") {
-      int d, m, y;
+      int d = 0;
+      int m = 0;
+      int y = 0;
       if (std::cin >> d >> m >> y) {
         if (current_period.is_empty) {
           std::cout << "<INVALID COMMAND>\n";
         } else {
-          loseva::Date limit; limit.d = d; limit.m = m; limit.y = y;
+          loseva::Date_t limit;
+          limit.d = d;
+          limit.m = m;
+          limit.y = y;
           loseva::push(history, current_period);
           if (loseva::compare_dates(current_period.end, limit) < 0) {
             current_period.is_empty = true;
@@ -150,10 +169,9 @@ int main(int argc, char** argv) {
     } else {
       std::cout << "<INVALID COMMAND>\n";
     }
-    std::string dummy;
+    std::string dummy = "";
     std::getline(std::cin, dummy);
   }
-  
   loseva::destroy(persons);
   loseva::destroy(meets);
   loseva::destroy(dataFiles);
